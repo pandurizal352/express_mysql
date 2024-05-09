2e2bbb3e171f9723779ef21c812ab09375b88ed0
Your Name <you@example.com>
2024-05-09 17:50:32 +0700

    modul-http-request

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var router = express.Router();
 //   res.send('respond with a resource');
 // });
 
-// kode yang di tambahkan di modul
+// kode yang di tambahkan di modul http requise
 router.get('/', function (req, res) {
   console.log(req.method)
   res.send('Halo Gamelab')
@@ -19,5 +19,68 @@ router.post('/', function (req, res) {
  res.send('Data sudah diterima')
 })
 
+router.get('/kueri', function (req, res) {
+  var nama = req.query.nama;
+  var usia = req.query.usia;
+  console.log(nama, usia)
+  res.json({nama, usia})
+ });
+
+ router.post('/users/:id', function (req, res) {
+  var id = req.params.id;
+  var path = req.path;
+  console.log(id, path)
+  res.json({id, path})
+ });
+
+// contoh 
+//  router.get('/', function (req, res) {
+//   console.log(req.headers)
+//   res.send('Selamat Datang Gamelab')
+//  })
+
+ router.get('/header', function (req, res) {
+  // memasukan data 
+  var userAgent = req.headers['user-agent'];
+  console.log(userAgent)
+  res.send(userAgent)
+ })
+
+ const users = {
+  gamelab: {
+  username: 'gamelab',
+  password: 'indonesia',
+  },
+ };
+ const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+  return res.status(401).json({ message: 'Tidak ada akses' });
+  }
+  const [type, credentials] = authHeader.split(' ');
+  if (type !== 'Basic') {
+  return res.status(401).json({ message: 'Tidak ada akses' });
+  }
+  const [username, password] = Buffer.from(credentials, 'base64')
+  .toString()
+  .split(':');
+  const user = users[username];
+  if (!user || user.password !== password) {
+  return res.status(401).json({ message: 'Tidak ada akses' });
+  }
+  req.auth = { username };
+  next();
+ };
+ router.use(authenticate);
+ router.get('/profile', (req, res) => {
+  res.json({
+  message: 'Selamat Datang di Gamelab Indonesia',
+  user: req.auth,
+  });
+ });
+
+
 
 module.exports = router;
+
+
